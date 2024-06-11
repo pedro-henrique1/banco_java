b14e9af38a7bbb6b23dc34236431ac663ee173e2
pedro henrique silva rodrigues <ph8464803@gmail.com>
2024-06-11 11:15:55 -0300

    modification

---
+++ src/ContaTerminal.java
@@ -0,0 +1,38 @@
+public class ContaTerminal {
+    int numConta ;
+    String agencia;
+    String nomeCliente;
+    double saldo;
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ContaTerminal novaConta = new ContaTerminal();
+
+        System.out.println("Bem vindo a sua conta");
+        Scanner contnum = new Scanner(System.in);
+        System.out.println("Digite seu nome");
+        novaConta.setNomeCliente(contnum.nextLine());
+        System.out.println("Digite o numero da agencia 000-8");
+        novaConta.setAgencia(contnum.next());
+        System.out.println("Digite o numero da conta");
+        novaConta.setNumConta(contnum.nextInt());
+        System.out.println("Digite o saldo");
+        novaConta.setSaldo(contnum.nextDouble());
+
+
+        System.out.println("Ola " + novaConta.getNomeCliente() + " obrigado por criar uma conta em nosso banco, sua agência é " + novaConta.getAgencia() + ", conta " + novaConta.getNumConta() + " e seu saldo " + novaConta.getSaldo() + " já está disponível para saque");
+
+
     }
 }
